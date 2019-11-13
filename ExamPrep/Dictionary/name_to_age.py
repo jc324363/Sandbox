3a393fe14dec21f67f7853751e8ffc6e359b728a
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-11-13 11:36:22 +1000

    Refactor name_to_age program

--- ExamPrep/Dictionary/name_to_age.py
+++ ExamPrep/Dictionary/name_to_age.py
@@ -5,5 +5,6 @@ add these to the dictionary, then display all of the data nicely.
 name_to_age = {"Bill": 21, "Jane": 4, "Jack": 56}
 name = input("Name: ")
 age = int(input("Age: "))
+name_to_age[name] = age
 for name, age in name_to_age.items():
-    print("{:6} - {:6}".format(name, age))
+    print("{:7} - {:6}".format(name, age))
