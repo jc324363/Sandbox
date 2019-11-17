b912a9df417e973bdf058aa3d81b311c53e5f1da
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-11-17 21:36:41 +1000

    Refactor dot_format program

--- ExamPrep/Strings/dot_format.py
+++ ExamPrep/Strings/dot_format.py
@@ -11,13 +11,6 @@ for number in range(0, 13, 3):
     print("{:02}".format(number))
 
 """
-Write a for loop that prints Olympic years (every 4 years) from 1900 to now
-"""
-
-for year in range(1900, 2020, 4):
-    print("{}".format(year))
-
-"""
 Write a print statement using the format method to print like:
 name = "Monty"
 money = 73.6
